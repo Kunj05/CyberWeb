1f1c0ea3be5d7343370d8541271a354d7036a8da
avengers2405 <akshitmishra24@gmail.com>
2024-10-01 03:15:12 +0530

    fresh repo

--- cyberweb/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-import Image from "next/image";
-// import Header from "./components/navbar";
-import { redirect } from "next/navigation";
-
-export default async function Home() {
-  redirect('/home');
-}
